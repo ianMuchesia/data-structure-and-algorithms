cb1118c3be5e0dcf95b8738720bf0897142117f2
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-24 11:14:13 +0300

    recursion

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,7 @@
 newArray = [1,2,3,4,5]
 
 const fibonacciFunc =(n)=>{
-    let fibArray = [0,1];
+    const fibArray = [0,1];
     for(let i=2;i<n;i++){
         fibArray.push(fibArray[fibArray.length-1] + fibArray[fibArray.length-2])
     }
@@ -9,7 +9,7 @@ const fibonacciFunc =(n)=>{
 
 
 }
-console.log(fibonacciFunc(7))
+//console.log(fibonacciFunc(7))
 
 //final solution
 
@@ -20,3 +20,16 @@ function fibonacci(n){
     }
     return fib;
 }
+
+//recursive solution
+const recursiveFibonacci=(n)=>{
+    let fibArray = [0,1];
+    if(n>2){
+        fibArray.push(recursiveFibonacci(n-1)[n-1] + recursiveFibonacci(n-2)[n-2]);
+        return fibArray;
+    }else{
+        return fibArray.slice(0,n);
+    }
+
+}
+console.log(recursiveFibonacci(3))
